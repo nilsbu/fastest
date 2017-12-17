988059c40ae3e66126acd19e58e85ff4c249d0c1
Nils Bussas <nils.bussas@gmail.com>
2017-12-17 21:14:04 +0100

    Some renaming + Nil()

--- require.go
+++ require.go
@@ -1,6 +1,6 @@
 package fastest
 
-//True asserts that cond is true.
+// True asserts that cond is true.
 func (ft T) True(cond bool, msg ...interface{}) {
 	if !cond {
 		if len(msg) == 0 {
@@ -11,8 +11,8 @@ func (ft T) True(cond bool, msg ...interface{}) {
 	}
 }
 
-//Nope asserts that cond is true.
-func (ft T) Nope(cond bool, msg ...interface{}) {
+//False asserts that cond is true.
+func (ft T) False(cond bool, msg ...interface{}) {
 	if cond {
 		if len(msg) == 0 {
 			ft.locatedFatalf("false != %v", cond)
@@ -22,8 +22,8 @@ func (ft T) Nope(cond bool, msg ...interface{}) {
 	}
 }
 
-// Same asserts that a and b are equal.
-func (ft T) Same(a interface{}, b interface{}, msg ...interface{}) {
+// Equals asserts that a and b are equal.
+func (ft T) Equals(a, b interface{}, msg ...interface{}) {
 	if a != b {
 		if len(msg) == 0 {
 			ft.TypedFatalf("%v != %v", a, b)
@@ -33,9 +33,9 @@ func (ft T) Same(a interface{}, b interface{}, msg ...interface{}) {
 	}
 }
 
-// Cond implements a -> b.
+// Implies implements a -> b.
 // That means it will only fail when a is false and b is true.
-func (ft T) Cond(a bool, b bool, msg ...interface{}) {
+func (ft T) Implies(a, b bool, msg ...interface{}) {
 	if a && !b {
 		if len(msg) == 0 {
 			ft.locatedFatalf("%v !-> %v", a, b)
@@ -54,8 +54,8 @@ func (ft T) Only(cond bool) {
 	}
 }
 
-// Null ..
-func (ft T) Null(a interface{}, msg ...interface{}) {
+// Nil asserts that parameter a is nil.
+func (ft T) Nil(a interface{}, msg ...interface{}) {
 	if a != nil {
 		if len(msg) == 0 {
 			ft.TypedFatalf("nil != %v", a)
